3a85c25f8a9524f7a62e9eb94dc082ece5c88088
mnaufalrifqir <mnaufalrifqir@gmail.com>
2024-09-30 21:26:25 +0700

    deploy app in vercel

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -9,10 +9,13 @@
   <!-- CSS for carousel/flickity-->
   <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
   <link rel="stylesheet" href="https://unpkg.com/flickity-fade@2/flickity-fade.css">
+
+  <link href="{{ asset('build/assets/app-BPhRPrHK.css') }}" rel="stylesheet">
+  <script src="{{ asset('build/assets/app-DI6-W-r-.js') }}" defer></script>
   
   <title>Java Juice Indonesia</title>
 
-  @vite('resources/css/app.css')
+  {{-- @vite('resources/css/app.css') --}}
   
   <!-- CSS for modal/flowbite -->
   <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" /> -->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Java Juice Indonesia</title>
-    @vite('resources/css/app.css')
+    <link href="{{ asset('build/assets/app-BPhRPrHK.css') }}" rel="stylesheet">
+    <script src="{{ asset('build/assets/app-DI6-W-r-.js') }}" defer></script>
+    {{-- @vite('resources/css/app.css') --}}
 </head>
 <body class="font-sans">
     @include('layouts.navbar')
